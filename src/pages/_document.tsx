7abbb3fe3bb721be794dbda0a58722def57069ac
mouttahidx <121966552+mouttahidx@users.noreply.github.com>
2024-03-21 00:35:27 +0000

    no index

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,7 @@ export default function Document() {
     <Html lang="fr">
       <Head>
         <ColorSchemeScript defaultColorScheme="auto" />
+        <meta name="robots" content="noindex"/>
       </Head>
       <body>
         <Main />
