1c5f0f6e6885e3dae59ab0bb9d7160fc2994db63
mouttahidx <121966552+mouttahidx@users.noreply.github.com>
2024-04-26 14:30:52 +0100

    deleted zip

--- app/_components/header/LanguageSelect.tsx
+++ app/_components/header/LanguageSelect.tsx
@@ -4,13 +4,13 @@ import Switcher from './Switcher';
 
 export default function LanguageSelect() {
   const locale = useLocale();
-  return (<></>
-    // <Switcher defaultValue={locale} label={"tests"}>
-    //   {locales.map((cur) => (
-    //     <option key={cur} value={cur} className='text-black'>
-    //       {cur}
-    //     </option>
-    //   ))}
-    // </Switcher>
+  return (
+    <Switcher defaultValue={locale} label={"tests"}>
+      {locales.map((cur) => (
+        <option key={cur} value={cur} className='text-black'>
+          {cur}
+        </option>
+      ))}
+    </Switcher>
   );
 }
