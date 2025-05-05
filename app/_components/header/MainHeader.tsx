e27a2fd972dcaee489f25d5953b095194a57ece8
AizazHSheerazi <hasanaizaz91@gmail.com>
2025-05-05 12:07:59 -0400

    Update MainHeader.tsx

--- app/_components/header/MainHeader.tsx
+++ app/_components/header/MainHeader.tsx
@@ -1,42 +1,16 @@
-"use client";
-
-import React, { useState, useEffect } from "react";
+import React from "react";
 import FreeQuote from "./FreeQuote";
 import Nav from "./Nav";
 import Logo from "./Logo";
-import MainHeader from "./MainHeader";
 
-export default function HeaderSwitcher() {
-  const [scrolled, setScrolled] = useState(false);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      setScrolled(window.scrollY > 50);
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
-
-  // Sticky Header (Background when scrolling)
-  const StickyHeader = () => (
-    <div className="bg-blue-600 w-full px-2 hidden lg:flex fixed top-0 justify-center z-50 shadow-md py-3">
-      <div className="flex justify-between w-full lg:max-w-6xl">
+export default function MainHeader() {
+  return (
+    <div className="bg-transparent w-full px-2 hidden lg:flex absolute top-14 justify-center z-10">
+      <div className="flex justify-between w-full lg:max-w-6xl pt-3">
         <Logo />
         <Nav />
         <FreeQuote />
       </div>
     </div>
   );
-
-  return (
-    <>
-      {/* Show MainHeader when not scrolled */}
-      {!scrolled && <MainHeader />}
-
-      {/* Show StickyHeader when scrolled */}
-      {scrolled && <StickyHeader />}
-    </>
-  );
 }
-
