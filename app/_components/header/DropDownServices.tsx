9ce42836aa17362909215a2ea9448c1396fc217a
AizazHSheerazi <hasanaizaz91@gmail.com>
2025-05-06 10:38:23 -0400

    Update DropDownServices.tsx

--- app/_components/header/DropDownServices.tsx
+++ app/_components/header/DropDownServices.tsx
@@ -1,1 +1,67 @@
+"use client";
 
+import { Button, Menu } from "@mantine/core";
+import NavigationLink from "./NavLink";
+import { Link } from "@/navigation";
+
+export default function DropDownServices({ translations }: { translations: any }) {
+  return (
+    <Menu
+      shadow="md"
+      position="bottom-start"
+      offset={10}
+      withArrow
+      trigger="click-hover"
+      openDelay={50}
+      closeDelay={200}
+      transitionProps={{ transition: "slide-up", duration: 250 }}
+    >
+      <Menu.Target>
+        <NavigationLink href={"/services"}>{translations['nav_services']}</NavigationLink>
+      </Menu.Target>
+      <Menu.Dropdown>
+        <Menu.Item
+          className="menu-item"
+          component={Link}
+          href="/services/agence-facebook-ads"
+        >
+          {translations['nav_fb']}
+        </Menu.Item>
+        <Menu.Divider />
+        <Menu.Item
+          className="menu-item"
+          component={Link}
+          href="/services/agence-google-ads"
+        >
+          {translations['nav_google']}
+        </Menu.Item>
+        <Menu.Divider />
+
+        <Menu.Item
+          className="menu-item"
+          component={Link}
+          href="/services/agence-marketing-digital"
+        >
+          {translations['nav_marketing']}
+        </Menu.Item>
+        <Menu.Divider />
+
+        <Menu.Item
+          className="menu-item"
+          component={Link}
+          href="/services/agence-seo"
+        >
+          {translations['nav_seo']}
+        </Menu.Item>
+      </Menu.Dropdown>
+      <Menu.Item
+          className="menu-item"
+          component={Link}
+          href="/services/via-crm"
+        >
+          {translations['nav_crm']}
+        </Menu.Item>
+      </Menu.Dropdown>
+    </Menu>
+  );
+}
